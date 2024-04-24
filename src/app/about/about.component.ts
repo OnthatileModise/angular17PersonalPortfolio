c61e0ff35e98dbd88d4b90d8b802e2a68b339b99
CAMPUS\Onthatile.Modise <onthatilem@interfile.co.za>
2024-04-24 20:46:05 +0200

    styling fixes

--- src/app/about/about.component.ts
+++ src/app/about/about.component.ts
@@ -3,11 +3,12 @@ import {Skills} from "../_models/Skills";
 import {SkillsService} from "../_services/skills.service";
 import {NgFor} from "@angular/common";
 import {state, style, trigger} from "@angular/animations";
+import { NgxTypedJsModule } from 'ngx-typed-js';
 
 @Component({
   selector: 'app-about',
   standalone: true,
-  imports: [NgFor],
+  imports: [NgFor , NgxTypedJsModule],
   templateUrl: './about.component.html',
   styleUrl: './about.component.css',
   animations : [trigger('scroll' , [
